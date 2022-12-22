bf320bbaa2edfbbca9b512da087c93e7f5464f00
brendan <brendanewen16@outook.com>
2022-12-22 08:49:34 +0000

    login page added

---
+++ auth/login.php
@@ -0,0 +1,95 @@
+<?php
+// "require" this file to output a form to add or edit data
+
+// For security, required PHP files should "die" if SAFE_TO_RUN is not defined
+if (!defined('SAFE_TO_RUN')) {
+    // Prevent direct execution - show a warning instead
+    die(basename(__FILE__)  . ' cannot be executed directly!');
+}
+?>
+
+<?php
+session_start(); 
+
+include "admin/connect.php";
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+
+    function validate($data){
+
+       $data = trim($data);
+
+       $data = stripslashes($data);
+
+       $data = htmlspecialchars($data);
+
+       return $data;
+
+    }
+
+    $uname = validate($_POST['username']);
+
+    $pass = validate($_POST['password']);
+
+    if (empty($uname)) {
+
+        header("Location: index.php?error=User Name is required");
+
+        exit();
+
+    }else if(empty($pass)){
+
+        header("Location: index.php?error=Password is required");
+
+        exit();
+
+    }else{
+
+        $sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) === 1) {
+
+            $row = mysqli_fetch_assoc($result);
+
+            if ($row['username'] === $uname && $row['password'] === $pass) {
+
+                echo "Logged in!";
+
+                $_SESSION['username'] = $row['username'];
+
+                $_SESSION['name'] = $row['name'];
+
+                $_SESSION['id'] = $row['id'];
+
+                header("Location: index.php");
+
+                exit();
+
+            }else{
+
+                header("Location: index.php?error=Incorect User name or password");
+
+                exit();
+
+            }
+
+        }else{
+
+            header("Location: index.php?error=Incorect User name or password");
+
+            exit();
+
+        }
+
+    }
+
+}else{
+
+    header("Location: index.php");
+
+    exit();
+
+}
+?>
---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+// Allow debugging
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Define a constant to permit each file we "require" to execute
+// For security, required PHP files should "die" if SAFE_TO_RUN is not defined
+define('SAFE_TO_RUN', true);
+
+// TODO: Change these values to configure the application
+$database_table = "user"; // name of database table to read/write
+$app_title = 'phptestsite - home';
+
+// Setup flags and other variables used in the application
+$url = $_SERVER["PHP_SELF"]; // URL of this page for forms to POST to
+$task = ''; // task to carry out in response to form submission
+// Data from data entry form
+$data = []; // key/value data from form submission
+
+
+// Define _e and _x functions
+require 'admin/helpers.php';
+// Output head of page
+require 'admin/head.php';
+?>
+
+<form action="auth/login.php" method="post">
+
+        <h2>LOGIN</h2>
+
+        <?php if (isset($_GET['error'])) { ?>
+
+            <p class="error"><?php echo $_GET['error']; ?></p>
+
+        <?php } ?>
+
+        <label>User Name</label>
+
+        <input type="text" name="username" placeholder="User Name"><br>
+
+        <label>Password</label>
+
+        <input type="password" name="password" placeholder="Password"><br> 
+
+        <button type="submit">Login</button>
+
+     </form>
+
+</body>
+
+</html>
